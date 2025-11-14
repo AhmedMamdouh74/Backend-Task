cfcd156e806c43b64df7151d9e348814b6967461
Ahmed Mamdouh <amamdouh823@gmail.com>
2025-11-14 14:27:56 +0200

    add dtos for Ipservice

---
+++ Application/DTOs/IpCheckResultDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class IpCheckResultDto
+    {
+
+        public string IpAddress { get; set; }
+        public string CountryCode { get; set; }
+        public bool IsBlocked { get; set; }
+
+    }
+}
---
+++ Domian/ReposContract/IBlockedAttemptLogRepo.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+
+namespace Domian.ReposContract
+{
+    public interface IBlockedAttemptLogRepo
+    {
+
+        Task AddLogAsync(BlockLog log);
+        Task<IEnumerable<BlockLog>> GetAllAsync();
+
+    }
+}
